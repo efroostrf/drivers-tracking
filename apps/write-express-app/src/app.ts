9ac0ee1f480013a5f23fd986c88c82d1b7b0a255
Yefrosynii Kolenko <contact@yefro.dev>
2025-11-23 23:11:07 +0200

    feat: install helmet for security

--- apps/write-express-app/src/app.ts
+++ apps/write-express-app/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 
 // routes
 import routes from "./routes";
@@ -9,6 +10,7 @@ import { errorHandler } from "./middleware/error-handler.middleware";
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 app.use(routes);
 app.use(errorHandler);
 
