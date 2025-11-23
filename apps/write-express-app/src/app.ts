acdad218cbfbe2c58ebb5ee726daf0128e852fd8
Yefrosynii Kolenko <contact@yefro.dev>
2025-11-23 23:14:19 +0200

    feat: disable x-powered-by and usage of custom 404

--- apps/write-express-app/src/app.ts
+++ apps/write-express-app/src/app.ts
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.disable("x-powered-by");
 app.use(routes);
 app.use(errorHandler);
+app.use((_req, res) => {
+  res.status(404).send();
+});
 
 export default app;
